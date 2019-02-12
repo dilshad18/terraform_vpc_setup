c1dbd4d8a19a39f469cd6875b9e6701436310c46
dilshad18 <34809568+dilshad18@users.noreply.github.com>
2019-02-12 22:59:54 +0530

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,13 @@
+resource "aws_vpc" "vpc" {
+        cidr_block = "10.218.32.0/20"
+        tags {
+         Name      = "pre-qa-vpc"
+        }
+}
+
+resource "aws_internet_gateway" "vpc_gateway" {
+        vpc_id    = "${aws_vpc.vpc.id}"
+        tags {
+         Name     = "vpc_igw"
+        }
+}
